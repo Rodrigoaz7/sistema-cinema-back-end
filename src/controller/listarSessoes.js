c1430b2e618948676b5006d5dd98cbaba652238e
Rodrigo Azevedo <rodrigo.aze7@gmail.com>
2019-06-12 21:27:43 -0300

    A filter example

--- src/controller/listarSessoes.js
+++ src/controller/listarSessoes.js
@@ -4,10 +4,17 @@ const Filme = require('./../models/Filme').filme;
 const sequelize = require('./../models').sequelize;
 
 exports.listarSessoes = async (req, res) => {
+
+    let query = "SELECT * FROM sessoes INNER JOIN filmes f ON f.id = sessoes.filme_id " +
+    " WHERE sessoes.cinema_id = " + req.params.idCinema;
+
+    if(req.query.classificacao_etaria) {
+        query += " AND f.classificacao_etaria >= " + req.query.classificacao_etaria;
+    }
+
     sessoes = await sequelize.query(
-        "SELECT * FROM sessoes INNER JOIN filmes ON filmes.id = sessoes.filme_id " +
-        " WHERE sessoes.cinema_id = " + req.params.idCinema, 
-        { type: sequelize.QueryTypes.SELECT}
+        query, { type: sequelize.QueryTypes.SELECT}
     )
+
     return res.status(200).json({sessoes: sessoes});
 }
